be5b16e9f5aa98f6186834ab898d25793480124b
mijime <07cc0110+gthb@gmail.com>
2016-06-18 13:52:04 +0900

    :pencil: Add lamma scripts

---
+++ source/study/kanazawa.rb/2016-06-18/scripts/my-function-js/index.js
@@ -0,0 +1,8 @@
+var through = require('through2');
+
+exports.handler = function(event, context) {
+  console.log('value1 =', event.key1);
+  console.log('value2 =', event.key2);
+  console.log('value3 =', event.key3);
+  context.succeed(event.key1);
+};
