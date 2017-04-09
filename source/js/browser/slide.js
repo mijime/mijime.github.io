12684717acf1a93ce35192eb5633bc5bbb82b0b4
mijime <07cc0110+gthb@gmail.com>
2017-04-09 13:08:18 +0900

    :+1: Update version

--- source/js/browser/slide.js
+++ source/js/browser/slide.js
@@ -1,9 +1,11 @@
+const revealCdn = 'cdnjs.cloudflare.com/ajax/libs/reveal.js/3.4.1';
+
 const printLink = document.createElement('link');
 printLink.rel = 'stylesheet';
 printLink.type = 'text/css';
 printLink.href = window.location.search.match(/print-pdf/)
-  ? '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0/css/print/pdf.min.css'
-  : '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0/css/print/paper.min.css';
+  ? `//${revealCdn}/css/print/pdf.min.css`
+  : `//${revealCdn}/css/print/paper.min.css`;
 document.querySelector('head').appendChild(printLink);
 
 const markdownTemplate = document.querySelector('[data-markdown] script[type=\'text/template\']');
@@ -16,34 +18,35 @@ Reveal.initialize({
   'theme':        Reveal.getQueryHash().theme,
   'transition':   Reveal.getQueryHash().transition || 'default',
   'dependencies': [{
-    'src':       '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0/lib/js/classList.min.js',
+    'src':       `//${revealCdn}/lib/js/classList.min.js`,
     'condition': () => !document.body.classList,
   }, {
-    'src':       '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0/plugin/markdown/marked.min.js',
+    'src':       `//${revealCdn}/plugin/markdown/marked.js`,
     'condition': () => !!markdownTemplate,
     'callback':  () => {
       const renderer = new marked.Renderer();
       const defaultRendererCode = renderer.code;
       renderer.code = function renderCode(code, language) {
-        if (language == 'mermaid') {
-          return '<div class=mermaid>' + code + '</div>';
+        if (language === 'mermaid') {
+          return `<div class="mermaid">${code}</div>`;
         }
+
         return defaultRendererCode.apply(this, arguments);
       };
       marked.setOptions({'renderer': renderer});
     }
   }, {
-    'src':       '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0/plugin/markdown/markdown.min.js',
+    'src':       `//${revealCdn}/plugin/markdown/markdown.min.js`,
     'condition': () => !!markdownTemplate,
   }, {
-    'src':      '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0/plugin/highlight/highlight.min.js',
+    'src':      `//${revealCdn}/plugin/highlight/highlight.min.js`,
     'async':    true,
     'callback': () => hljs.initHighlightingOnLoad(),
   }, {
-    'src':   '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0/plugin/zoom-js/zoom.min.js',
+    'src':   `//${revealCdn}/plugin/zoom-js/zoom.min.js`,
     'async': true
   }, {
-    'src':   '//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0/plugin/notes/notes.min.js',
+    'src':   `//${revealCdn}/plugin/notes/notes.min.js`,
     'async': true
   }]
 });
