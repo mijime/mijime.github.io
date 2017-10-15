d0e73d94edde96d24703b8772ab6eb9989adb1db
mijime <07cc0110+gthb@gmail.com>
2017-10-15 22:48:05 +0900

    Add Pelican scripts

---
+++ scripts/pelican/pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+SITEURL = "https://mijime.github.io/til"
+SITENAME = "mijime's til"
+AUTHOR = "mijime"
+BIO = "today i learning"
+
+PATH = "content"
+
+TIMEZONE = "Asia/Tokyo"
+DEFAULT_LANG = "ja"
+
+THEME = "themes/mdl-blog"
+
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_CATEGORY = "misc"
+DEFAULT_PAGINATION = 10
+
+ARTICLE_URL = "posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html"
+
+FEED_ALL_ATOM = "feeds/atom.xml"
+CATEGORY_FEED_ATOM = "feeds/category-%s.atom.xml"
+TRANSLATION_FEED_ATOM = "feeds/translation-%s.atom.xml"
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
