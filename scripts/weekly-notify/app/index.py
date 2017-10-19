babb0b6d26e8146aaf124d21853f9a3034dc31af
mijime <07cc0110+gthb@gmail.com>
2017-10-20 07:44:06 +0900

    python2っぽく書く

--- scripts/weekly-notify/app/index.py
+++ scripts/weekly-notify/app/index.py
@@ -8,7 +8,7 @@ import boto3
 import itertools
 import math
 import os
-import functools
+from functools import reduce
 
 UNIT_YEAR_TO_SEC = 365 * 24 * 60 * 60
 UNIT_MONTH_TO_SEC = 30 * 24 * 60 * 60
@@ -59,7 +59,7 @@ def fetch_trend_repolist(tag="", since="daily"):
         title = title_anchor.getText().strip()
         url = "https://github.com" + title_anchor.get("href").strip()
         descHtml = repo.select(".py-1 p")
-        desc = functools.reduce(lambda x, y: x + y, \
+        desc = reduce(lambda x, y: x + y, \
                 map(lambda x: x.getText().strip(), descHtml), "")
         yield TRENDS_FORMAT.format(title=title, url=url, desc=desc)
 
