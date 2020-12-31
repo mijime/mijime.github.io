87c77e2c00b80c5fa5c2e600456eecb5c863d15c
mijime <07cc0110+gthb@gmail.com>
2021-01-01 03:21:43 +0900

    変換する前にメタデータを補正する

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -9,11 +9,16 @@ interface Metadata {
 export function buildMetadataByFrontMatter(data: {
   [key: string]: any
 }): Metadata {
-  return {
+  const originMetadata = {
     title: data.title || data.Title || '',
     description: data.description || data.Description || '',
     date: data.date || data.Date || new Date().toISOString(),
     tags: data.tags || data.Tags || [],
     draft: !!data.draft || !!data.Draft
+  }
+  return {
+    ...originMetadata,
+    date: new Date(originMetadata.date).toISOString(),
+    tags: originMetadata.tags.map((tag: string) => tag.toLowerCase())
   }
 }
