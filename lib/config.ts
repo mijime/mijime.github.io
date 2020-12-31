d5b23545a7ea8b3d8b5bb29a27a49ae0a4a50e5d
mijime <07cc0110+gthb@gmail.com>
2021-01-01 02:25:49 +0900

    Add generate sitemap script

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,6 @@
 export const LANG = 'ja'
 export const SITE_NAME = 'My snippets'
 export const PAGE_SIZE = 5
+export const SITE_URL = 'https://mijime.github.io'
 export const GA_TRACKING_ID = 'UA-46554348-4'
 export const SITE_VERIFICATION = '7gVj5rzyozu0vYcQMLhIGr4g-WWyJn4R22RgeYqQdS0'
