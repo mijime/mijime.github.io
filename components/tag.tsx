fd92abfe2b065e27dc14ecd15eab8def27f90b32
mijime <07cc0110+gthb@gmail.com>
2021-01-01 16:39:29 +0900

    Refactor path names

--- components/tag.tsx
+++ components/tag.tsx
@@ -7,7 +7,7 @@ type TagProps = {
 export default function Tag({ tag }: TagProps) {
   return (
     <span className="tag is-rounded is-info">
-      <Link href={`/tag/${tag}/1`}>{tag}</Link>
+      <Link href={`/tag/${tag}/posts/1`}>{tag}</Link>
     </span>
   )
 }
