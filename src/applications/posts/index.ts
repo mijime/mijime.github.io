00a5e0278194874247c3b5e4ac01f7efbdeeb380
mijime <07cc0110+gthb@gmail.com>
2021-01-08 13:55:34 +0900

    Draft機能を有効にする

--- src/applications/posts/index.ts
+++ src/applications/posts/index.ts
@@ -1,4 +1,6 @@
 import { MarkdownPostsRepository } from '@/infrastructures/repositories/markdown-posts/'
 import { PostsUsecase } from '@/usecases/posts/'
 
-export const PostsApp: PostsUsecase = new MarkdownPostsRepository()
+export const PostsApp: PostsUsecase = new MarkdownPostsRepository({
+  isProduction: process.env.NODE_ENV === 'production'
+})
