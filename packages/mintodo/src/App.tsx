8cbcb276f824caa7ba6a273c05dad52990f4d24c
mijime <mijime@users.noreply.github.com>
2026-06-22 09:49:38 +0900

    feat(mintodo): mount DslEditorModal in app shell

--- packages/mintodo/src/App.tsx
+++ packages/mintodo/src/App.tsx
@@ -3,6 +3,7 @@ import { BoardDeleteDialog } from "./components/BoardDeleteDialog";
 import { BoardNameDialog } from "./components/BoardNameDialog";
 import { BoardSidebar } from "./components/BoardSidebar";
 import { Canvas } from "./components/Canvas";
+import { DslEditorModal } from "./components/DslEditorModal";
 import { EditModal } from "./components/EditModal";
 import { EmptyState } from "./components/EmptyState";
 import { HelpModal } from "./components/HelpModal";
@@ -63,6 +64,7 @@ function Shell() {
       <StatsPanel />
       <ShortcutHint />
       <EditModal />
+      <DslEditorModal />
       <HelpModal />
       <BoardNameDialog />
       <BoardDeleteDialog />
